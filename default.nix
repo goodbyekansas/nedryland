20db3fec961afbe8dedb56eeb04b2bbbded53c95
Albert Cervin <albert@acervin.com>
2020-03-20 10:57:01 +0100

    👷 Refactored deployment
    - Add support for nix-shell
    - Add support for nedryland to use a simple declarative
    configuration format

--- default.nix
+++ default.nix
@@ -1,94 +1,69 @@
-{ projectName, defaultConfigFile, componentPaths }:
+{ name, configFile, componentInitFn }:
 let
-  mozilla = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  python_pkg_overlays = import ./overlays/python_packages.nix;
-  pkgs = import (
-    builtins.fetchGit {
-      name = "nixpackages-20.03-beta";
-      url = "https://github.com/NixOS/nixpkgs.git";
-      ref = "refs/tags/20.03-beta"; # pinned because of https://github.com/NixOS/nixpkgs/issues/75941
-    }
-  )
+  pkgs = import ./nixpkgs.nix
     {
       overlays =
         [
-          mozilla
-          python_pkg_overlays
+          # rust
+          (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+
+          # extra pip packages
+          (import ./overlays/python_packages.nix)
         ];
     };
 
-  k8sFunctions = import ./configs/k8sconfig.nix pkgs;
-  #protos = import ./protocols/default.nix pkgs;  # TODO
-  configContentFromEnv = builtins.getEnv "${pkgs.lib.toUpper projectName}_config";
+  configContentFromEnv = builtins.getEnv "${pkgs.lib.toUpper name}_config";
   configContent = if configContentFromEnv != "" then configContentFromEnv else (
-    if builtins.pathExists defaultConfigFile then builtins.readFile defaultConfigFile else "{}"
+    if builtins.pathExists configFile then builtins.readFile configFile else "{}"
   );
+
+  # gbk-pipeline specific functionality
   base = {
-    mkComponent = { package, deployment ? {}, docs ? null, pth ? builtins.toString ./. }: rec { inherit package deployment docs pth; };
-    mkStaticHTMLContainer = import ./utils/build-static-nginx.nix pkgs;
-    mkK8sConfigStaticImage = k8sFunctions.static;
-    mkK8sConfig = k8sFunctions.dynamic;
-    mkDatabaseConfig = import ./configs/databaseconfig.nix;
-    mkDeploymentConfig = import ./configs/deploymentconfig.nix;
+    mkComponent = { package, deployment ? {}, docs ? null }: rec { inherit package deployment docs; };
+    deployment = pkgs.callPackage ./deployment.nix {};
     theme = import ./theme/default.nix pkgs;
-    #protos = protos.package;
-    parseConfig = import ./nix/config.nix pkgs configContent (pkgs.lib.toUpper projectName);
+    parseConfig = import ./config.nix pkgs configContent (pkgs.lib.toUpper name);
   };
 
-  allComponents = builtins.listToAttrs (
-    builtins.map (
-      pth: {
-        name = builtins.baseNameOf (builtins.dirOf pth);
-        value = {
-          pth = builtins.toString pth;
-          component =
-            let
-              c = pkgs.callPackage pth { inherit base; };
-            in
-              c // {
-                packageWithChecks = c.package.overrideAttrs (
-                  oldAttrs: {
-                    doCheck = true;
-                  }
-                );
-              };
-        };
-      }
-    ) componentPaths
-  );
+  components = componentInitFn {
+    nedryland = {
+      declareComponent = path: { dependencies ? {} }:
+        let
+          c = pkgs.callPackage path ({ inherit base; } // dependencies);
+        in
+          c // {
+            inherit path;
+            packageWithChecks = c.package.overrideAttrs (
+              oldAttrs: {
+                doCheck = true;
+              }
+            );
+          };
+    };
+  };
+
+  allComponents = (builtins.attrValues components);
 in
 rec {
-  all = builtins.map (v: v.component) (builtins.attrValues allComponents);
-  package = builtins.map (component: component.package) all;
-  packageWithChecks = builtins.map (component: component.packageWithChecks) all;
+  inherit components; # Find a better way to communicate this with the shells!
+
+  package = builtins.map (component: component.package) allComponents;
+  packageWithChecks = builtins.map (component: component.packageWithChecks) allComponents;
   deploymentConfigs = builtins.filter (c: c != null)
-    (builtins.map (component: component.deployment) all);
+    (builtins.map (component: component.deployment) allComponents);
   docs = pkgs.lib.foldl (x: y: x // y) {} (
     builtins.filter (d: d != {} && d != null)
-      (builtins.map (component: component.docs) all)
+      (builtins.map (component: component.docs) allComponents)
   );
 
   deploy =
     {
       local = pkgs.callPackage ./infra/local.nix {
-        inherit base deploymentConfigs;
+        inherit deploymentConfigs base;
       };
       prod = pkgs.callPackage ./infra/prod.nix {
-        inherit base deploymentConfigs;
+        inherit deploymentConfigs base;
       };
     };
 
-  shells = {
-    deploy = {
-      local = pkgs.callPackage ./infra/local/shell.nix {
-        deployment = deploy.local;
-      };
-      prod = pkgs.callPackage ./infra/prod/shell.nix {
-        deployment = deploy.prod;
-      };
-    };
-  };
-
-  inherit allComponents;
-
-} // (builtins.mapAttrs (n: v: v.component) allComponents)
+} // components
