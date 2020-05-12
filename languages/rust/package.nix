53a1d906bbd041b210904caf3352ff1248ecdbcd
Sakarias Johansson <sakariasjohansson@hotmail.com>
2020-05-12 16:25:58 +0200

    ⚡ Add temp folder for wasi when using test for rust projects

--- languages/rust/package.nix
+++ languages/rust/package.nix
@@ -157,8 +157,18 @@ pkgs.stdenv.mkDerivation (
     } else { }
   ) // (
     if defaultTarget == "wasm32-wasi" then {
-      # run the tests through virtual vm
-      CARGO_TARGET_WASM32_WASI_RUNNER = "wasmer run --env=RUST_TEST_NOCAPTURE=1";
+      # run the tests through virtual vm, create a temp directory and map it to the vm
+      CARGO_TARGET_WASM32_WASI_RUNNER = pkgs.writeTextFile {
+        name = "runner.sh";
+        executable = true;
+        text = ''
+          temp_dir=$(mktemp -d)
+          wasmer run --env=RUST_TEST_NOCAPTURE=1 --mapdir=:$temp_dir "$@"
+          exit_code=$?
+          rm -rf $temp_dir
+          exit $exit_code
+        '';
+      };
     } else { }
   )
 )
