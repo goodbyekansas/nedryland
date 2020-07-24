e837f6bc4bd4ba91fe15ab7e3aff99a7f3ef6dd8
Sakarias Johansson <sakariasjohansson@hotmail.com>
2020-07-24 11:41:46 +0200

    ⚡️ Build wheel as separate derivation
    The wheel is built as a separate derivation and uses a setup hook to
    insert a "link file" into the original derivation. This keeps the
    dependency proper and does not introduce "eval-time dependencies"

--- languages/python/default.nix
+++ languages/python/default.nix
@@ -14,25 +14,52 @@ rec {
     , doStandardTests ? true
     }:
     let
-      package = mkPackage { inherit name version pythonVersion checkInputs buildInputs nativeBuildInputs propagatedBuildInputs preBuild src doStandardTests; };
-
-      packageWithWheel = package // {
-        wheel = pythonVersion.pkgs.buildPythonPackage {
-          pname = "${name}-wheel";
-          format = "other";
-
-          inherit src;
-
-          buildPhase = ''
-            ${pythonVersion} setup.py bdist_wheel
-          '';
-
-          installPhase = ''
-            mkdir -p $out
-            cp dist/*.whl $out/
-          '';
-        };
+      package = mkPackage {
+        inherit
+          name
+          version
+          pythonVersion
+          checkInputs
+          buildInputs
+          nativeBuildInputs
+          propagatedBuildInputs
+          preBuild
+          src
+          doStandardTests;
       };
+
+      wheel = package.overrideAttrs (oldAttrs: {
+        name = "${oldAttrs.name}-wheel";
+        format = "other";
+
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+          pythonVersion.pkgs.wheel
+          pythonVersion.pkgs.pip
+        ];
+
+        # TODO: this currently builds a wheel for the host platform,
+        # not the target one if that is needed. Building for the host platform
+        # works well enough for python-only wheels.
+        buildPhase = ''
+          eval "$preBuild"
+          ${oldAttrs.buildPhase or ""}
+          python setup.py bdist_wheel || echo "Python utilities must support building wheels (bdist_wheel)"
+          eval "$postBuild"
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp dist/*.whl $out/
+        '';
+
+        # setup hook that creates a "link" file in the
+        # derivation that depends on this wheel derivation
+        setupHook = ./wheelHook.sh;
+      });
+
+      packageWithWheel = package.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ [ wheel ];
+      });
     in
     base.mkComponent {
       package = packageWithWheel;
