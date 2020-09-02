fe6f2d67b35b0cdbc7412cbec2790a2b850b9175
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2020-09-02 09:38:02 +0200

    🐛 Return correct exit codes from terraform alias

--- languages/terraform/default.nix
+++ languages/terraform/default.nix
@@ -54,8 +54,10 @@
             subcommand="$1"
             if [ "$subcommand" == "apply" ] || [ "$subcommand" == "plan" ]; then
                command terraform "$@" -var-file=/tmp/tfvars.json
+               return $?
             else
                command terraform "$@"
+               return $?
             fi
           }
 
@@ -66,11 +68,14 @@
             ${if disableApplyInShell then ''
             if [ $# -gt 0 ] && [ "$subcommand" == "apply" ]; then
               echo "Local 'apply' has been disabled, which probably means that application of Terraform config is done centrally"
+              return 1
             else
               terraform_with_args "$@"
+              return $?
             fi
           '' else ''
             terraform_with_args "$@"
+            return $?
           ''}
             ${postTerraformHook}
           }
