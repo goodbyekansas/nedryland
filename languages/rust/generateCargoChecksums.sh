433ea194fb19fb7d1907f6619dcc144d291e2208
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2020-11-06 14:38:48 +0100

    ✨ Add rust vendoring
    This adds two derivations that rust packages can depend on, one for
    internal dependencies and one for external. These derivations create
    what cargo calls vendor directories. These can be searched by Cargo
    instead of crates.io. We use cargo vendor to create a derivation for
    crates.io dependencies. However, cargo does not really support creating
    a vendor directory with unpublished crates (like we need to for internal
    dependencies). Therefore, we have reverse-engineered the format of a
    cargo vendor directory and create one for our internal dependencies by
    simply unpacking the .crate file that cargo generates (a tar.gz) and
    then generate the needed .cargo-checksum.json in the unpacked directory.
    
    After those two derivations have been created, we use setup hooks in nix
    to generate a .cargo/config.toml that redirects cargo to them. For our internal
    vendor directory we use a fake registry that is then redirected to a
    local folder. Cargo seems to accept this even though it is said to be
    unsupported.
    
    To Nedryland rust packages, this means that they can depend on internal
    rust packages by specifying them in `buildInputs` or
    `propagatedBuildInputs` and then adding a `Cargo.toml` dependency with
    the registry set to "nix" like this:
    
        firm-types = { version = "0.1.0", registry = "nix" }
    
    Currently we do not support if you have your own Cargo config at
    .cargo/config.toml and will tell you to remove it if it is there.
    Inclusion of custom config may be added later if/when needed.
    
    Furthermore, at the moment vendoring of external dependencies happens
    only for applications, not for libraries. This is since the normal build
    artifact of a rust library is the source code for it, i.e. it is not
    built. We do however build libraries in their check phases so we could
    look into using vendoring there.

---
+++ languages/rust/generateCargoChecksums.sh
@@ -0,0 +1,13 @@
+generateCargoChecksums() {
+  echo "  🖨 generating checksums for $name."
+  for crate in $out/src/rust/*; do
+    if [ ! -f "$crate/.cargo-checksum.json" ]; then
+      (cd $crate; find . -type f -exec sha256sum {} \; | \
+        jq -Rn '{files: (reduce inputs as $val ({}; . + ($val | split("\\s+"; "")  | {(.[1]): (.[0])})))}' > \
+        ../.cargo-checksum.json; mv ../.cargo-checksum.json .)
+    fi
+  done
+  echo "  🖨 checksums generated!"
+}
+
+fixupOutputHooks+=(generateCargoChecksums)
---
+++ languages/rust/vendor.nix
@@ -0,0 +1,167 @@
+pkgs: rust: { src, name, buildInputs, propagatedBuildInputs }:
+let
+  internalSetupHook = pkgs.makeSetupHook
+    {
+      name = "internal-deps-hook";
+    }
+    ./internalDepsSetupHook.sh;
+
+  # derivation that creates a fake vendor dir
+  # with our internal nix dependencies
+  internal = pkgs.stdenv.mkDerivation {
+    name = "${name}-internal-deps";
+    inherit src buildInputs propagatedBuildInputs;
+
+    phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+    nativeBuildInputs = with pkgs; [ git cacert rust internalSetupHook ];
+
+    buildPhase = ''
+      export CARGO_HOME=$PWD
+
+      if [ -n "''${rustDependencies}" ]; then
+        echo "🏡 vendoring internal dependencies..."
+
+        mkdir -p vendored
+
+        # symlink in all deps
+        for dep in $rustDependencies; do
+          ln -sf "$dep" ./vendored/
+        done
+
+        echo "🏡 internal dependencies vendored!"
+      fi
+    '';
+
+    installPhase = ''
+      mkdir $out
+
+      if [ -d vendored ]; then
+        cp -r vendored $out
+
+        substitute ${./cargo-local.config.toml} $out/cargo.config.toml \
+          --subst-var-by vendorDir $out/vendored
+
+        mkdir -p "$out/nix-support"
+        substituteAll "$setupHook" "$out/nix-support/setup-hook"
+      fi
+    '';
+
+    impureEnvVars = pkgs.stdenv.lib.fetchers.proxyImpureEnvVars;
+    setupHook = ./setupHook.sh;
+  };
+
+  # a derivation that checks that Cargo.lock is up to date
+  # and generates an up-to-date one if it is not
+  upToDateCargoLock = pkgs.stdenv.mkDerivation {
+    name = "${name}-Cargo.lock";
+    inherit src;
+
+    phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+
+    nativeBuildInputs = with pkgs; [ git cacert rust coreutils internal ];
+
+    preBuild = ''
+      if declare -f createCargoConfig > /dev/null; then
+        createCargoConfig
+      fi
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+      export CARGO_HOME=$PWD
+
+      cp Cargo.lock Cargo.lock.orig
+
+      # this will contact crates.io to
+      # check if the lock file is up to date
+      # w.r.t. what is specified in the manifest (Cargo.toml)
+      # We also run all commands with -q because the output
+      # from a successful run is not all that helpful and
+      # error output will still be printed
+      if cargo update -q --locked; then
+        echo "🔏 👍 Cargo.lock is up to date"
+      else
+        echo "🔏 🗓 Cargo.lock is out of date, generating a new one..."
+        cargo update -q
+
+        echo "An up-to-date Cargo.lock for \"${name}\" has been generated at $out"
+      fi
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      cp Cargo.lock $out
+    '';
+
+    impureEnvVars = pkgs.stdenv.lib.fetchers.proxyImpureEnvVars;
+  };
+
+  # a derivation that generates a "vendor" dir with
+  # all internal and crates.io dependencies
+  external = pkgs.stdenv.mkDerivation {
+    name = "${name}-external-deps";
+    inherit src upToDateCargoLock;
+
+    phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+
+    nativeBuildInputs = with pkgs; [ git cacert rust internal ];
+
+    preBuild = ''
+      if declare -f createCargoConfig > /dev/null; then
+        createCargoConfig
+      fi
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+      export CARGO_HOME=$PWD
+
+      # check to see if Cargo.lock is up to date
+      # the reason we do not fail the above derivation
+      # if it isn't is to be able to provide the user
+      # with an up-to-date Cargo.lock that they can
+      # use
+      if ! cmp Cargo.lock $upToDateCargoLock; then
+        echo "Cargo.lock is not up to date for \"$name\"! You can use the one at $upToDateCargoLock"
+        exit 1
+      fi
+
+      # We need to set this so that
+      # cargo vendor will generate timestamps
+      # that corresponds to those that will be in the
+      # nix store (unix epoch 1). Otherwise the generated
+      # checksums will change and cargo will not be able
+      # to use the vendored packages
+      export SOURCE_DATE_EPOCH=1
+
+      echo "🌍 vendoring dependencies from crates.io..."
+
+      # we cannot really run with --locked here because
+      # that would require the internal dependencies to be
+      # part of Cargo.toml. However, the validity of the lockfile
+      # will still be guaranteed by the dependency on the upToDateCargoLock
+      # derivation
+      cargo vendor -q --versioned-dirs --respect-source-config vendored
+
+      echo "🌍 dependencies from crates.io vendored!"
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      mkdir $out
+
+      cp -r vendored $out
+
+      substitute ${./cargo.config.toml} $out/cargo.config.toml \
+        --subst-var-by vendorDir $out/vendored
+
+      mkdir -p "$out/nix-support"
+      substituteAll "$setupHook" "$out/nix-support/setup-hook"
+    '';
+
+    setupHook = ./setupHook.sh;
+
+    impureEnvVars = pkgs.stdenv.lib.fetchers.proxyImpureEnvVars;
+  };
+in
+{ inherit internal external; }
