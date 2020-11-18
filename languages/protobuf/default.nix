bf1f04508c000e8929768a47afbb6ed349da37ef
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2020-11-18 11:00:08 +0100

    ⚡ Add protoInputs and includeServices to protobuf.mkModule
    `protoInputs` sets dependencies on other protobuf modules.

--- languages/protobuf/default.nix
+++ languages/protobuf/default.nix
@@ -3,7 +3,7 @@ let
   supportedLanguages = [ "rust" "python" ];
 in
 rec {
-  generateCode = { name, src, version, languages ? supportedLanguages, includeServices ? false }:
+  generateCode = { name, src, version, languages ? supportedLanguages, includeServices ? false, protoIncludePaths ? [ ] }:
     let
       invariantSource = if pkgs.lib.isStorePath src then src else builtins.path { path = src; inherit name; };
     in
@@ -14,7 +14,7 @@ rec {
             p = ./. + "/${l}.nix";
             f = if builtins.pathExists p then (import p) else builtins.abort "👺 Language ${l} is not supported";
             args = builtins.intersectAttrs (builtins.functionArgs f) {
-              inherit name rust includeServices version;
+              inherit name rust includeServices version protoIncludePaths;
               protoSources = invariantSource;
             };
           in
@@ -25,22 +25,25 @@ rec {
         languages
     );
 
-  mkModule = { name, src, version, languages ? supportedLanguages, pythonVersion ? pkgs.python3 }:
+  mkModule = { name, src, version, languages ? supportedLanguages, pythonVersion ? pkgs.python3, includeServices ? false, protoInputs ? [ ] }:
     let
-      withServices = generateCode { inherit name src languages version; includeServices = true; };
-      onlyMessages = generateCode { inherit name src languages version; includeServices = false; };
+      generatedCode = generateCode {
+        inherit name src languages version includeServices;
+        protoIncludePaths = builtins.map (pi: pi.protobuf) protoInputs;
+      };
     in
     {
+      protobuf = src;
       python = python.mkUtility {
         inherit name version pythonVersion;
-        src = withServices.python;
-        propagatedBuildInputs = (pypkgs: [ pypkgs.grpcio ]);
+        src = generatedCode.python;
+        propagatedBuildInputs = (pypkgs: [ pypkgs.grpcio ] ++ builtins.map (pi: pi.python.package) protoInputs);
         doStandardTests = false; # We don't want to run our strict tests on generated code and stubs
       };
 
-      rust = {
-        withServices = rust.mkUtility { inherit name version; src = withServices.rust; };
-        onlyMessages = rust.mkUtility { inherit name version; src = onlyMessages.rust; };
+      rust = rust.mkUtility {
+        inherit name version;
+        src = generatedCode.rust;
       };
     };
 
--- languages/protobuf/python.nix
+++ languages/protobuf/python.nix
@@ -1,46 +1,44 @@
-{ stdenv, name, version, protoSources, python3 }:
+{ stdenv, name, version, protoSources, python3, protoIncludePaths }:
 stdenv.mkDerivation {
-  inherit protoSources;
+  inherit protoSources protoIncludePaths;
   name = "python-${name}";
   src = ./python;
   packageName = builtins.replaceStrings [ "-" ] [ "_" ] name;
   nativeBuildInputs = with python3.pkgs; [ grpcio-tools mypy-protobuf mypy setuptools ];
   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
   buildPhase = ''
-    mkdir -p ./$packageName
 
-    substitute ./setup.py.in ./setup.py --subst-var-by packageName ${name} --subst-var-by version ${version}
+    shopt -s globstar extglob nullglob
+
+    substituteInPlace ./setup.py --subst-var-by packageName ${name} --subst-var-by version ${version}
+
+    includes=""
+    for p in $protoIncludePaths; do
+      includes+=" -I $p"
+    done
 
     python -m grpc_tools.protoc \
         -I "$protoSources" \
-        --python_out=./$packageName \
-        --grpc_python_out=./$packageName \
-        --mypy_out=./$packageName \
+        $includes \
+        --python_out=. \
+        --grpc_python_out=. \
+        --mypy_out=. \
         "$protoSources"/**/*.proto
 
     # protoc does not add __init__.py files, so let's do so
-    find ./$packageName -type d -exec touch {}/__init__.py \;
-    find ./$packageName -type d -exec touch {}/py.typed \;
+    find . -type d -exec touch {}/__init__.py \;
+    find . -type d -exec touch {}/py.typed \;
 
-    shopt -s globstar
-    shopt -s extglob
-    shopt -s nullglob
-
-    for pyfile in ./$packageName/**/*_grpc.py; do
+    for pyfile in ./**/*_grpc.py; do
       stubgen $pyfile -o .
       # Correcting some mistakes made by stubgen.
       # Generate static methods without return types. We just replace that with any return type.
       sed -i -E 's/\):/\) -> Any:/' ''${pyfile}i
     done
-
-    # correct the imports since that is apparently impossible to do correctly
-    sed -i -E "s/^from (\S* import .*_pb2)/from $packageName.\1/ " $packageName/**/*.py
-    sed -i -E "s/^from (\S* import .*_pb2)/from $packageName.\1/ " $packageName/**/*.pyi
-    sed -i -E "s/^from (\S*.*_pb2)/from $packageName.\1/ " $packageName/**/*.pyi
   '';
 
   installPhase = ''
     mkdir -p $out
-    cp -r ./$packageName ./setup.py $out
+    cp -r . $out
   '';
 }
