ccbce2759d20967bdff06f1081b89c460ee2a410
Albert Cervin <albert@acervin.com>
2020-11-27 07:37:46 +0100

    🔥 Remove vendoring of external dependencies
    Internal dependencies are still "vendored" like before.
    
    It adds unneccesary complexity at this point in the project and cargo
    vendor, with the requirements it has on the up-to-dateness of
    Cargo.lock, makes vendoring inconvenient.
    
    Instead, we should use Cargo.lock as an input to create a series of
    fixed output derivations (the hash is in Cargo.lock) followed by a
    derivation to create a vendor directory from these.

--- languages/rust/protobuf/compiler/default.nix
+++ languages/rust/protobuf/compiler/default.nix
@@ -3,5 +3,4 @@ mkClient {
   name = "rust-protobuf-compiler";
   src = ./.;
   PROTOC = "${protobuf}/bin/protoc";
-  externalDependenciesHash = "sha256-HYFVW8Du8SKmfSFlMfk/bsLSkEE7bCRpvmyM53L5zLA=";
 }
