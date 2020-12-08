61c11fbbc06a957acd4fe5252b1f69d2503852cc
Albert Cervin <albert@acervin.com>
2020-12-08 09:50:57 +0100

    ⬆️⚡️ Update nixpkgs and build wasilibc with clang 11
    Also, add debugging support to it and create a clang 11 stdenv for
    wasi.

--- overlays/wasilibc.nix
+++ overlays/wasilibc.nix
@@ -1,12 +1,59 @@
 self: super:
+let
+  # set this to true to produce a wasilibc with debug symbols
+  # TODO: there is probably a better place for this
+  debug = false;
+in
 {
-  wasilibc = super.wasilibc.overrideAttrs (oldAttrs: {
+  # create statically built versions of the llvm libraries (wasi does not support dynamic linking)
+  llvmPackages_11 = super.llvmPackages_11 // {
+    libraries = super.llvmPackages_11.libraries // rec {
+      libcxxabi = super.llvmPackages_11.libraries.libcxxabi.override {
+        enableShared = false;
+      };
+      libcxx = super.llvmPackages_11.libraries.libcxx.override {
+        enableShared = false;
+        inherit libcxxabi;
+      };
+    };
+  };
+
+  # convenience stdenv that uses clang 11 for wasi
+  clang11Stdenv = (super.overrideCC super.stdenv super.buildPackages.llvmPackages_11.lldClang);
+
+  # override wasilibc with a newer version that is compiled with clang 11
+  wasilibc = (super.wasilibc.override {
+    stdenv = (super.overrideCC super.stdenv super.buildPackages.llvmPackages_11.lldClangNoLibc);
+  }).overrideAttrs (oldAttrs: {
     name = "wasilibc-20201202";
     src = self.fetchFromGitHub {
-      owner = "WebAssembly";
+      owner = "goodbyekansas"; # TODO: change to upstream after https://github.com/WebAssembly/wasi-libc/pull/226 is in
       repo = "wasi-libc";
-      rev = "378fd4b21aab6d390f3a1c1817d53c422ad00a62";
-      sha256 = "0h5g0q5j9cni7jab0b6bzkw5xm1b1am0dws2skq3cc9c9rnbn1ga";
+      rev = "b601c8509d3230b3abff3d12264b8c752ec3299c";
+      sha256 = "0142kbdjdlj8d5hnmr65f5bpwfskfyfw7wv7g3f58cl4y4r367b8";
     };
-  });
+
+    # we need to add two -isystem flags due to nix purity. The clang in Nix does not add any
+    # standard include paths (i.e. $sysroot/include) is not added as an include path. This used to
+    # be the case for clang 8 but not anymore for 9+
+  } // (if debug then {
+    # we need to remove the check that defines will match up
+    # since they won't for debug
+    patches = [
+      ./remove-define-check.patch
+    ];
+
+    # do not do _any_ stripping
+    dontStrip = true;
+    dontStripTarget = true;
+    dontStripHost = true;
+
+    makeFlagsArray = oldAttrs.makeFlagsArray or [ ] ++ [
+      "WASM_CFLAGS=-g -isystem ../sysroot/include -isystem sysroot/include"
+    ];
+  } else {
+    makeFlagsArray = oldAttrs.makeFlagsArray or [ ] ++ [
+      "WASM_CFLAGS=-O2 -DNDEBUG -isystem ../sysroot/include -isystem sysroot/include"
+    ];
+  }));
 }
