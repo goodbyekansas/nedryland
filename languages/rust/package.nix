bd9d848e5d759d3616375b07cad5b143b4cff393
Albert Cervin <albert@acervin.com>
2021-01-27 13:56:17 +0100

    ⚡ Allow overriding wasi runner for rust

--- languages/rust/package.nix
+++ languages/rust/package.nix
@@ -195,17 +195,19 @@ stdenv.mkDerivation (
   ) // (
     if defaultTarget == "wasm32-wasi" then {
       # run the tests through virtual vm, create a temp directory and map it to the vm
-      CARGO_TARGET_WASM32_WASI_RUNNER = pkgs.writeTextFile {
-        name = "runner.sh";
-        executable = true;
-        text = ''
-          temp_dir=$(mktemp -d)
-          wasmer run --env=RUST_TEST_NOCAPTURE=1 --mapdir=:$temp_dir "$@"
-          exit_code=$?
-          rm -rf $temp_dir
-          exit $exit_code
-        '';
-      };
+      CARGO_TARGET_WASM32_WASI_RUNNER = (
+        attrs.CARGO_TARGET_WASM32_WASI_RUNNER or (pkgs.writeTextFile {
+          name = "runner.sh";
+          executable = true;
+          text = ''
+            temp_dir=$(mktemp -d)
+            wasmer run --env=RUST_TEST_NOCAPTURE=1 --mapdir=:$temp_dir "$@"
+            exit_code=$?
+            rm -rf $temp_dir
+            exit $exit_code
+          '';
+        })
+      );
     } else { }
   ) // (
     if warningsAsErrors then {
