5518b5035022d86ef1ff3734655d00503b2c9dad
Sakarias Johansson <sakariasjohansson@hotmail.com>
2021-02-05 15:39:03 +0100

    ⚡ Change apply doCheck and install only if darwin

--- overlays/python_packages.nix
+++ overlays/python_packages.nix
@@ -121,10 +121,11 @@ in
 
           # these tests seems broken for python 3.8 on macos
           # https://hydra.nixos.org/job/nixpkgs/nixpkgs-20.09-darwin/python38Packages.python-language-server.x86_64-darwin
-          python-language-server = super.python-language-server.overrideAttrs (oldAttrs: {
-            doCheck = !isDarwin;
-            doInstallCheck = !isDarwin;
-          });
+          python-language-server = super.python-language-server.overrideAttrs (oldAttrs:
+            (if isDarwin then {
+              doCheck = false;
+              doInstallCheck = false;
+            } else { }));
 
         };
       };
