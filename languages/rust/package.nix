afa7d8ba92685c568f7fd9c8eacfa6fea784815d
Albert Cervin <albert.cervin@goodbyekansas.com>
2021-06-15 12:06:16 +0200

    ⚡ Expose vendored dependencies on rust drvs
    This is needed to be able to build it without Nix (i.e. natively on
    other Linux distributions)

--- languages/rust/package.nix
+++ languages/rust/package.nix
@@ -172,6 +172,7 @@ stdenv.mkDerivation
       strictDeps = true;
       disallowedReferences = [ vendor ];
       src = invariantSource;
+      vendoredDependencies = vendor;
 
       nativeBuildInputs = with pkgs; [
         cacert
