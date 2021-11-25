db7a45c1c5a2f27c6a6b8a749bc9f6a723301a70
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2021-11-25 15:11:20 +0100

    🐛 Give python srcExclude the same thing as base
    Python doesn't use base.mkDerivation so it didn't get the earlier fix.

--- examples/documentation/client/client.nix
+++ examples/documentation/client/client.nix
@@ -4,6 +4,10 @@ base.languages.python.mkClient {
   version = "1.0.0";
   pythonVersion = python38;
   src = ./.;
+  srcExclude = [
+    (path: type: (type == "directory" && baseNameOf path == "manual"))
+    (path: type: (type == "regular" && baseNameOf path == "about.md"))
+  ];
   docs = {
     # mkMdbook and mkDocs can take either a folder or a single file as source
     manual = base.languages.markdown.mkDocs {
