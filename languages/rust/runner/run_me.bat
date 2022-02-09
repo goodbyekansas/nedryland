6f51c00e0b4b3d55f954be350bfd3c08b146b1c5
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2022-02-09 16:11:14 +0100

    🔨 Restructure rust runners
    They are now derivations with setup hooks instead of just attrsets. This
    makes them a bit more logical to author while the user experience stays
    the same.

--- languages/rust/runner/run_me.bat
+++
@@ -1,1 +0,0 @@
-Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File A:\install-openssh.ps1' -Verb RunAs}"
--- languages/rust/runner/wasi.nix
+++ languages/rust/runner/wasi.nix
@@ -1,17 +1,23 @@
-{ hostPlatform, writeTextFile, attrs, ... }:
-if hostPlatform == "wasm32-wasi" then {
-  # run the tests through virtual vm, create a temp directory and map it to the vm
-  CARGO_TARGET_WASM32_WASI_RUNNER = (
-    attrs.CARGO_TARGET_WASM32_WASI_RUNNER or (writeTextFile {
-      name = "runner.sh";
-      executable = true;
-      text = ''
-        temp_dir=$(mktemp -d)
-        wasmer run --env=RUST_TEST_NOCAPTURE=1 --mapdir=:$temp_dir "$@"
-        exit_code=$?
-        rm -rf $temp_dir
-        exit $exit_code
-      '';
-    })
-  );
-} else { }
+{ writeTextFile, wasmer, makeSetupHook }:
+makeSetupHook
+{
+  name = "wasi-runner-hook";
+  substitutions = {
+    runner =
+      # run the tests through virtual vm, create a temp directory and map it to the vm
+      writeTextFile {
+        name = "runner.sh";
+        executable = true;
+        text = ''
+          temp_dir=$(mktemp -d)
+          ${wasmer}/bin/wasmer run --env=RUST_TEST_NOCAPTURE=1 --mapdir=:$temp_dir "$@"
+          exit_code=$?
+          rm -rf $temp_dir
+          exit $exit_code
+        '';
+      };
+  };
+}
+  (builtins.toFile "wasi-runner-hook" ''
+    export CARGO_TARGET_WASM32_WASI_RUNNER=@runner@
+  '')
