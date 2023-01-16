97ea2ad8c4492d35eb66bfaa8644de2244ba0fac
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2023-01-16 09:42:31 +0100

    💎 Release Nedryland 8.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [8.1.1] - 2023-01-16
+
 ### Fixed
 - Help message for entering a shell without a proper target.
 
--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ let
       "Nedryland supports nixpkgs versions >= 22.05, you have ${pkgs.lib.version or "unknown"}}";
     let
       pkgs' = pkgs.extend gitIgnoreOverlay;
-      version = "8.0.0";
+      version = "8.1.1";
       versionAtLeast = pkgs'.lib.versionAtLeast version;
     in
     {
