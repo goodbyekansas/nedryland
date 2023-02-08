de46d2e7db2eb46cfaeaa9056e7553d665a7d447
Sakarias Johansson <sakarias.johansson@goodbyekansas.com>
2023-02-08 16:46:02 +0100

    💎 Release 8.3.0

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ let
       "Nedryland supports nixpkgs versions >= 22.05, you have ${pkgs.lib.version or "unknown"}}";
     let
       pkgs' = pkgs.extend gitIgnoreOverlay;
-      version = "8.2.1";
+      version = "8.3.0";
       versionAtLeast = pkgs'.lib.versionAtLeast version;
     in
     {
