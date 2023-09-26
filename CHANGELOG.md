5a755070e25a5c29eb772f46881fd1fca0699a68
paulbarton90 <28630076+paulbarton90@users.noreply.github.com>
2023-09-26 16:32:39 +0100

    💎 Release Nedryland 8.3.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [8.3.3] - 2023-09-26
+
 ### Fixed
 - mdbook shell commands now finds the running process correctly, making the stop
   and open commands work.
--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ let
       "Nedryland supports nixpkgs versions >= 22.05, you have ${pkgs.lib.version or "unknown"}}";
     let
       pkgs' = pkgs.extend gitIgnoreOverlay;
-      version = "8.3.1";
+      version = "8.3.3";
       versionAtLeast = pkgs'.lib.versionAtLeast version;
     in
     {
